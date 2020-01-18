05b0149b11b660f435bad6ccbba815c097a08655
desireetorrente <desiree.torrente@gmail.com>
2020-01-18 14:37:18 +0100

    Mostrando post desde la BD

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,9 @@ from django.utils import timezone
 
 # Create your models here.
 
-# models.Model significa que Post es un modelo de Django, 
+# models.Model significa que Post es un modelo de Django,
 # así Django sabe que debe guardarlo en la base de datos
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -16,6 +17,6 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-    
     def __str__(self):
         return self.title
+        
